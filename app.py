052f4040404aa9728a8a04f0299492d7310f17fa
Sanjaykumarpeace <peacebillioner@gmail.com>
2026-05-06 06:56:56 +0530

    added google genai

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
-from flask import Flask, render_template, request 
+from flask import Flask, render_template, request
+from google import genai
 app = Flask(__name__)
+
+# Add your API key
+genai.configure(api_key="AIzaSyAjtbpYAbTKqtTajbK_SPhdYW8-EDNKnDY")
+
+model = genai.GenerativeModel("gemini-pro")
 
 @app.route('/')
 def home():
@@ -8,8 +14,24 @@ def home():
 @app.route('/summarize', methods=['POST'])
 def summarize():
     text = request.form['text']
-    #Temporary response
-    summary = "This is a sample summary of your text."
+    mode = request.form['mode']
+
+    if mode == "summary":
+        prompt = f"Give a short summary of this text:\n{text}"
+    elif mode == "teacher":
+        prompt = f"Explain this like a teacher in simple terms:\n{text}"
+    elif mode == "exam":
+        prompt = f"Give important exam key points from this:\n{text}"
+    else:
+        prompt = text
+
+    try:
+        response = model.generate_content(prompt)
+        summary = response.text
+    except Exception as e:
+        summary = f"Error: {str(e)}"
+
     return render_template('index.html', summary=summary)
+
 if __name__ == '__main__':
     app.run(debug=True)
